edeb3b9405797f2d71747d2d1d1636c9076fb9d5
PIJAGBeer <alexandre.bazzi@gmail.com>
2015-06-08 21:20:21 -0300

    cliente, clienteDao, clienteTableModel, clienteController, update na classe conexao

--- src/br/senai/sc/jagbeer/controller/ClienteController.java
+++ src/br/senai/sc/jagbeer/controller/ClienteController.java
@@ -2,35 +2,97 @@ package br.senai.sc.jagbeer.controller;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 
 import br.senai.sc.jagbeer.abstracts.Entidade;
+import br.senai.sc.jagbeer.dao.ClienteDAO;
 import br.senai.sc.jagbeer.interfaces.IController;
+import br.senai.sc.jagbeer.model.Cliente;
 
-public class ClienteController implements IController{
+public class ClienteController implements IController {
+
+	ClienteDAO dao = new ClienteDAO();
 
 	@Override
 	public void salvar(Entidade entidade) {
-		// TODO Auto-generated method stub
-		
+
+		// Fazer a verificação se há numeros no nome na view
+		try {
+			Cliente cliente = (Cliente) entidade;
+
+			if (cliente == null)
+				throw new Exception("Cliente não pode ser nulo.");
+
+			if (cliente.getNome().trim().equals(""))
+				throw new Exception("Nome do Cliente obrigatório");
+
+			dao.salvar(cliente);
+
+		} catch (Exception e) {
+			System.out.println("[ClienteController] - Erro ao Salvar Cliente"
+					+ e.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"[ClienteController] - Erro ao Salvar Cliente");
+		}
+
 	}
 
 	@Override
 	public void excluir(Entidade entidade) {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			Cliente cliente = (Cliente) entidade;
+
+			if (cliente == null)
+				throw new Exception("Selecione ao menos um cliente.");
+			dao.excluir(cliente);
+		} catch (Exception e) {
+			System.out.println("[ClienteController] - Erro ao Excluir Cliente"
+					+ e.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"[ClienteController] - Erro ao Salvar Cliente");
+		}
+
 	}
 
 	@Override
 	public void editar(Entidade entidade) {
-		// TODO Auto-generated method stub
-		
+
+		try {
+			Cliente cliente = (Cliente) entidade;
+
+			if (cliente == null)
+				throw new Exception("Cliente não pode ser nulo.");
+
+			if (cliente.getNome().trim().equals(""))
+				throw new Exception("Nome do Cliente obrigatório");
+
+			dao.salvar(cliente);
+
+		} catch (Exception e) {
+			System.out.println("[ClienteController] - Erro ao Editar Cliente"
+					+ e.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"[ClienteController] - Erro ao Editar Cliente");
+		}
+
 	}
 
 	@Override
 	public List<Entidade> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Entidade> listEntidade = null;
+		try {
+			listEntidade = dao.listar();
+		} catch (Exception e) {
+			System.out
+					.println("[ClienteController] - Erro ao listar clientes. "
+							+ e.getMessage());
+			JOptionPane.showMessageDialog(null,
+					"[ClienteController] - Erro ao listar clientes. ");
+		}
+
+		return listEntidade;
 	}
 
 	@Override
@@ -41,8 +103,12 @@ public class ClienteController implements IController{
 
 	@Override
 	public void atualizaTabela(JTable table) {
-		// TODO Auto-generated method stub
-		
+		try {
+			dao.atualizaTabela(table);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
